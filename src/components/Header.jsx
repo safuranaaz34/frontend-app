aecdf232e00b2e2dbd0be6d384dcfec98342c9e4
safuranaaz34 <safuranaaz34@gmail.com>
2026-03-13 11:57:42 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,34 @@
-import "./Header.css"
+import "./Header.css";
 import { Link } from "react-router-dom";
+import { AppContext } from "../App";
+import { useContext } from "react";
 function Header() {
+  const { user } = useContext(AppContext);
   return (
     <div className="App-Header">
       <h1>My Store</h1>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/Cart">Cart</Link></li>
-        <li><Link to="/Orders">Orders</Link></li>
-        <li><Link to="/Login">Login</Link></li>
-        <li><Link to="/Logout">Logout</Link></li>
-    
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart</Link>
+        </li>
+        <li>
+              <Link to="/orders">Order</Link>
+            </li>
+        {user?.email ? (
+          <>
+            
+            <li>
+              <Link to="/logout">Logout</Link>
+            </li>
+          </>
+        ) : (
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        )}
       </ul>
     </div>
   );
